373bf0728c8487230de6c32c0038cd9c9d172803
iyeahh <yboravender@gmail.com>
2024-10-08 11:25:06 +0900

    init

---
+++ SeSACPbxConflict/SeSACPbxConflictApp.swift
@@ -0,0 +1,17 @@
+//
+//  SeSACPbxConflictApp.swift
+//  SeSACPbxConflict
+//
+//  Created by Bora Yang on 10/8/24.
+//
+
+import SwiftUI
+
+@main
+struct SeSACPbxConflictApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
